9dc7adb92c694db9d17ad999f3ec83a7cc6e2574
Mixassss <bigboommaximandsim@gmail.com>
2024-12-19 11:52:54 -0500

    create folder

---
+++ basedate/include/utility.h
@@ -0,0 +1,13 @@
+#ifndef utility_h
+#define utility_h
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <functional>
+
+
+#endif // UTILITY_H
